51e4aaf5c4ee8bc7bf9a49b73cacef58c40d7da1
Kilian Koeltzsch <me@kilian.io>
2017-10-01 17:27:55 +0200

    update to Swift 4 package syntax

--- Package.swift
+++ Package.swift
@@ -1,5 +1,22 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "GaussKrueger"
+    name: "GaussKrueger",
+    products: [
+        .library(
+            name: "GaussKrueger",
+            targets: ["GaussKrueger"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "GaussKrueger",
+            dependencies: [],
+            path: "Sources/"),
+        .testTarget(
+            name: "GaussKruegerTests",
+            dependencies: ["GaussKrueger"]),
+    ]
 )
